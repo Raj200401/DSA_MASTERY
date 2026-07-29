36394b9c1bda489330b406a1438fd782f0d96aa7
Raj <codingfreak202527@gmail.com>
2026-07-29 09:51:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,44 +1,44 @@
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
         // code here
-        ArrayList<Integer>result=new ArrayList<>();
+      ArrayList<Integer> result=new ArrayList<>();
       int left=0;
       int right=0;
       while(left<a.length && right<b.length){
           if(a[left]<b[right]){
               if(result.isEmpty()  || result.get(result.size()-1)!=a[left]){
-                  result.add(a[left]);
+              result.add(a[left]);
               }
               left++;
           }else if(a[left]>b[right]){
               if(result.isEmpty()  || result.get(result.size()-1)!=b[right]){
-                  result.add(b[right]);
+              result.add(b[right]);
               }
               right++;
           }else{
-              if(result.isEmpty()  || result.get(result.size()-1)!=b[right]){
-                  result.add(b[right]);
+              
+             if(result.isEmpty()  || result.get(result.size()-1)!=a[left]){
+              result.add(a[left]);
               }
-              right++;
               left++;
+              right++;
           }
       }
       
-        
       while(left<a.length){
-              if(result.isEmpty()  || result.get(result.size()-1)!=a[left]){
-                  result.add(a[left]);
+          if(result.isEmpty()  || result.get(result.size()-1)!=a[left]){
+              result.add(a[left]);
               }
               left++;
       }
       
       while(right<b.length){
-              if(result.isEmpty()  || result.get(result.size()-1)!=b[right]){
-                  result.add(b[right]);
+         if(result.isEmpty()  || result.get(result.size()-1)!=b[right]){
+              result.add(b[right]);
               }
               right++;
       }
       
-     return result;
-    }
+      return result;
+}
 }
