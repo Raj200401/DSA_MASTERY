66bfa3e4e53b0311a9e26137b0e0664d1564c3c9
Raj <codingfreak202527@gmail.com>
2026-08-04 11:18:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.java
@@ -1,47 +1,44 @@
 class Solution {
     public static ArrayList<Integer> findUnion(int a[], int b[]) {
         // code here
-    ArrayList<Integer> result=new ArrayList<>();
-    int left=0;
-    int right=0;
-    while(left<a.length && right<b.length){
-        if(a[left]<b[right]){
-            
-        if(result.isEmpty() || result.get(result.size()-1)!=a[left]){
-            result.add(a[left]);
-        }
+        ArrayList<Integer> result=new ArrayList<>();
+        int left=0;
+        int right=0;
+        while(left<a.length && right<b.length){
+            if(a[left]<b[right]){
+            if(result.isEmpty() || result.get(result.size()-1)!=a[left]){
+                result.add(a[left]);
+            }
             left++;
-        }else if(a[left]>b[right]){
-            
-             if(result.isEmpty() || result.get(result.size()-1)!=b[right]){
-            result.add(b[right]);
-             }
+            }else if(a[left]>b[right]){
+                if(result.isEmpty() || result.get(result.size()-1)!=b[right]){
+                result.add(b[right]);
+            }
             right++;
-        }else{
-            
-             if(result.isEmpty() || result.get(result.size()-1)!=a[left]){
-            result.add(a[left]);
-             }
+            }else{
+                if(result.isEmpty() || result.get(result.size()-1)!=a[left]){
+                result.add(a[left]);
+            }
             left++;
             right++;
+            }
         }
+        
+        while(left<a.length){
+            if(result.isEmpty() || result.get(result.size()-1)!=a[left]){
+                result.add(a[left]);
+            }
+            left++;
+        }
+        
+        while(right<b.length){
+            if(result.isEmpty() || result.get(result.size()-1)!=b[right]){
+                result.add(b[right]);
+            }
+            right++;
+        }
+        
+        
+        return result;
     }
-    
-    while(left<a.length){
-         if(result.isEmpty() || result.get(result.size()-1)!=a[left]){
-        result.add(a[left]);
-         }
-        left++;
-    }
-    
-    while(right<b.length){
-         if(result.isEmpty() || result.get(result.size()-1)!=b[right]){
-        result.add(b[right]);
-         }
-        right++;
-    }
-    
-    
-    return result;
-}
 }
