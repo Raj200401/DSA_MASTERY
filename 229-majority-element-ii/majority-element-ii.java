eb156433c546faad14a73a002ecb406a4485478b
Raj <codingfreak202527@gmail.com>
2026-08-18 10:53:59 +0530

    Time: 2 ms (99.96%) | Memory: 52.9 MB (22.10%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,42 +2,54 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
         int count1=0;
-        int element1=0;
         int count2=0;
-        int element2=0;
-
+        int ele1=0;
+        int ele2=0;
+        List<Integer> result=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(count1==0 && nums[i]!=element2){
-                count1=1;
-                element1=nums[i];
-            }else if(count2==0 && nums[i]!=element1){
-                count2=1;
-                element2=nums[i];
-            }else if(nums[i]==element1){
+            if(count1==0 && nums[i]!=ele2){
+                ele1=nums[i];
                 count1++;
-            }else if(nums[i]==element2){
+            }else if(count2==0 && nums[i]!=ele1){
+                ele2=nums[i];
                 count2++;
-
+            }else if(ele1==nums[i]){
+                count1++;
+            }else if(ele2==nums[i]){
+                count2++;
             }else{
                 count1--;
                 count2--;
             }
         }
 
-        List<Integer> list=new ArrayList<>();
+        int c=0;
+        int req=n/3+1;
+        if (ele1==ele2){
+            for(int i=0;i<n;i++){
+                if(nums[i]==ele1){
+                    c++;
+                }
+            }
+        }
+        if(c>=req) {
+            result.add(ele1);
+        return result;
+        }
         int c1=0;
         int c2=0;
-        int min=n/3+1;
         for(int i=0;i<n;i++){
-            if(element1==nums[i]){
+            if(nums[i]==ele1){
                 c1++;
-            }else if(element2==nums[i]){
+            }
+        }
+        if(c1>=req) result.add(ele1);
+        for(int i=0;i<n;i++){
+            if(nums[i]==ele2){
                 c2++;
             }
         }
-
-        if(c1>=min) list.add(element1);
-        if(c2>=min) list.add(element2);
-        return list;
+        if(c2>=req) result.add(ele2);
+        return result;
     }
 } 
