bebd068306c9f463658ff81b364e9d2cf1756136
Raj <codingfreak202527@gmail.com>
2026-08-20 09:35:49 +0530

    Time: 3 ms (72.84%) | Memory: 52.5 MB (53.43%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,21 +1,22 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        ArrayList<Integer> result=new ArrayList<>();
         int n=nums.length;
         int count1=0;
-        int count2=0;
         int ele1=0;
+        int count2=0;
         int ele2=0;
-        List<Integer> result=new ArrayList<>();
+
         for(int i=0;i<n;i++){
-            if(count1==0 && nums[i]!=ele2){
+            if(count1==0 && ele2!=nums[i]){
                 ele1=nums[i];
                 count1++;
             }else if(count2==0 && nums[i]!=ele1){
                 ele2=nums[i];
                 count2++;
-            }else if(ele1==nums[i]){
+            }else if(nums[i]==ele1){
                 count1++;
-            }else if(ele2==nums[i]){
+            }else if(nums[i]==ele2){
                 count2++;
             }else{
                 count1--;
@@ -23,24 +24,21 @@ class Solution {
             }
         }
 
-        
         int req=n/3+1;
-        
-        
         int c1=0;
         int c2=0;
+
         for(int i=0;i<n;i++){
             if(nums[i]==ele1){
                 c1++;
-            }
-        }
-        if(c1>=req) result.add(ele1);
-        for(int i=0;i<n;i++){
-            if(nums[i]==ele2){
+            }else if(nums[i]==ele2){
                 c2++;
             }
         }
-        if(c2>=req && ele1!=ele2) result.add(ele2);
+
+        if(c1>=req) result.add(ele1);
+        if(c2>=req) result.add(ele2);
+
         return result;
     }
 } 
