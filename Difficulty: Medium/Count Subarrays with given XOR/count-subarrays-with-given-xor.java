3c1515ade27045aa4a67c0ba8b7940e8dba75313
Raj <codingfreak202527@gmail.com>
2026-08-20 09:58:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -1,19 +1,23 @@
 class Solution {
     public long subarrayXor(int arr[], int k) {
-       
-       int n=arr.length;
-       int xor=0;
-       long count=0;
-       
-       HashMap<Integer,Integer> mpp=new HashMap<>();
-       mpp.put(0,1);
-       for(int i=0;i<n;i++){
-           xor=xor^arr[i];
-           
-           int req=xor^k;
-           count+=mpp.getOrDefault(req,0);
-           
-           mpp.put(xor,mpp.getOrDefault(xor,0)+1);
-       }
-       return count;
-}}
+        int n=arr.length;
+        int count=0;
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+        int xor=0;
+        mpp.put(0,1);
+        for(int i=0;i<n;i++){
+            xor=xor^arr[i];
+            
+            int req=xor^k;
+            count+=mpp.getOrDefault(req,0);
+            
+            mpp.put(xor,mpp.getOrDefault(xor,0)+1);
+            
+            
+            
+        }
+        
+        return count;
+}
+
+}
