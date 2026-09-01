5f7e72208d03eb5fdae95135827e56fbc10f43c9
Raj <codingfreak202527@gmail.com>
2026-09-01 11:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (50.12%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,23 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n=nums.length;
-        int low=0;
-        int high=n-1;
-        
-        while(low<=high){
-            int mid=(low+high)/2;
+        return recursive(nums,0,n-1,target);
+    }
 
-            if(target==nums[mid]){
-                return mid;
-            }
+    public int recursive(int []nums,int low,int high,int target){
 
-            if(target<nums[mid]){
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-
-        return -1;
+        if(low>high) return -1;
+       int mid=(low+high)/2;
+       if(nums[mid]==target) return mid;
+       if(nums[mid]>target){
+        return recursive(nums,low,mid-1,target);
+       }else{
+        return recursive(nums,mid+1,high,target);
+       }
+       
     }
 }
