29f2b0393e050fdefc2c5c053d726bb74978bd30
Raj <codingfreak202527@gmail.com>
2026-09-02 11:53:09 +0530

    Time: 75 ms (28.68%) | Memory: 59.2 MB (77.22%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -2,47 +2,52 @@ class Solution {
     public int reversePairs(int[] nums) {
         return divide(nums,0,nums.length-1);
     }
-    public int divide(int nums[],int low,int high){
+    public int divide(int []nums,int low,int high){
         int count=0;
-        if(low>=high) return 0;
+        if(low==high) return 0;
         int mid=(low+high)/2;
         count+=divide(nums,low,mid);
         count+=divide(nums,mid+1,high);
         count+=merge(nums,low,mid,high);
         return count;
+
     }
-    public int merge(int nums[],int low,int mid,int high){
-        int right=mid+1;
+
+    public int merge(int []nums,int low,int mid,int high){
         int count=0;
+        int right=mid+1;
+
         for(int left=low;left<=mid;left++){
             while(right<=high && nums[left]>2L*nums[right]){
                 right++;
             }
+
             count+=right-(mid+1);
         }
+
         int left=low;
-        right=mid+1;
-        
-        ArrayList<Integer>rs=new ArrayList<>();
+         right=mid+1;
+        ArrayList<Integer> arr=new ArrayList<>();
         while(left<=mid && right<=high){
+            
             if(nums[left]<=nums[right]){
-                rs.add(nums[left++]);
+                arr.add(nums[left++]);
             }else{
-                rs.add(nums[right++]);
+                arr.add(nums[right++]);
             }
         }
         while(left<=mid){
-            rs.add(nums[left++]);
+            arr.add(nums[left++]);
         }
+
         while(right<=high){
-            rs.add(nums[right++]);
+            arr.add(nums[right++]);
         }
 
         for(int i=low;i<=high;i++){
-            nums[i]=rs.get(i-low);
+            nums[i]=arr.get(i-low);
         }
+
         return count;
     }
-
-
 }
