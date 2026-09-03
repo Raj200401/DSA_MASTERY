4a923df5197b062858abfa5c52a980b0a6ac9722
Raj <codingfreak202527@gmail.com>
2026-09-03 11:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (98.89%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -5,13 +5,13 @@ class Solution {
         int high=n-1;
         while(low<=high){
             int mid=(low+high)/2;
-            
-             if(target==nums[mid]){
+
+            if(target==nums[mid]){
                 return mid;
-            }else if(target>nums[mid]){
-                low=mid+1;
-            }else{
+            }else if(target<nums[mid]){
                 high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return low;
