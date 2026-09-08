8d7c16b8abbc063bc27adc08195a56ed399ce139
Raj <codingfreak202527@gmail.com>
2026-09-08 10:58:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (65.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,30 +1,29 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        return recursive(nums,target,0,n-1);
-    }
+       int n=nums.length;
+       return recursive(nums,target,0,n-1);
+}
+public int recursive(int[]nums,int target,int low,int high){
+     
+     if(low>high) return -1;
+     int mid=(low+high)/2;
 
-    public int recursive(int[]nums,int target,int low,int high){
-        if(low>high) return -1;
-
-        int mid=(low+high)/2;
-
-        if(nums[mid]==target){
-            return mid;
-        }else{
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<=nums[mid]){
-                    return recursive(nums,target,low,mid-1);
-                }else{
-                    return recursive(nums,target,mid+1,high);
-                }
+     if(nums[mid]==target){
+        return mid;
+     }else{
+        if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<nums[mid]){
+                return recursive(nums,target,low,mid-1);
             }else{
-                if(nums[mid]<=target && target<=nums[high]){
-                    return recursive(nums,target,mid+1,high);
-                }else{
-                    return recursive(nums,target,low,mid-1);
-                }
+                return recursive(nums,target,mid+1,high);
+            }
+        }else{
+            if(nums[mid]<target && target<=nums[high]){
+                return recursive(nums,target,mid+1,high);
+            }else{
+                return recursive(nums,target,low,mid-1);
             }
         }
-    }
+     }
+}
 }
