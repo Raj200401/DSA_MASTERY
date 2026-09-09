921cd14f9ef4fc82cd12cd811806c85e90c5dabf
Raj <codingfreak202527@gmail.com>
2026-09-09 12:28:01 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (12.19%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,33 +1,36 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         int n=nums.length;
-        return recursive(nums,target,0,n-1);
-    }
-    public boolean recursive(int[]nums,int target,int low,int high){
-        if(low>high) return false;
-        int mid=(low+high)/2;
+        int low=0;
+        int high=n-1;
 
-        if(nums[mid]==target){
-            return true;
-        }else if(nums[low]==nums[mid] && nums[mid]==nums[high]){
-            low++;
-            high--;
-            return recursive(nums,target,low,high);
-        }
-        else{
-            if(nums[low]<=nums[mid]){
-                if(nums[low]<=target && target<nums[mid]){
-                    return recursive(nums,target,low,mid-1);
+        while(low<=high){
+            int mid=(low+high)/2;
+            
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low++;
+                high--;
+                
+            }
+            else{
+                if(nums[mid]>=nums[low]){
+                    if(nums[low]<=target && target<nums[mid]){
+                        high=mid-1;
+                    }else{
+                        low=mid+1;
+                    }
                 }else{
-                    return recursive(nums,target,mid+1,high);
-                }
-            }else{
-                if(nums[mid]<target &&target<=nums[high]){
-                    return recursive(nums,target,mid+1,high);
-                }else{
-                    return recursive(nums,target,low,mid-1);
+                    if(nums[mid]<target && target<=nums[high]){
+                        low=mid+1;
+                    }else{
+                        high=mid-1;
+                    }
                 }
             }
         }
+      return false;
     } 
 }
