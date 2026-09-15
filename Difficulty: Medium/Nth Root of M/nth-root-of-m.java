dd465187c723da667616653380343bb5fdf49eb5
Raj <codingfreak202527@gmail.com>
2026-09-15 11:43:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Nth Root of M/nth-root-of-m.java
+++ Difficulty: Medium/Nth Root of M/nth-root-of-m.java
@@ -1,25 +1,25 @@
 class Solution {
     public int nthRoot(int n, int m) {
         // code here
-        
-        int low=1;
-        int high=m;
-       if(m==0)return 0;
-        while(low<=high){
-            int mid=(low+high)/2;
-             long val=1;
-            for(int i=0;i<n;i++){
-                val*=mid;
-                if(val>m) break;
-            }
-            if(val==m){
-                return mid;
-            }else if(val>m){
-                high=mid-1;
-                }else{
-                low=mid+1;
-            }
-        }
-        return -1;
+        if(m==0)return 0;
+     int low=1;
+     int high=m;
+     while(low<=high){
+         int mid=low+(high-low)/2;
+         long val=1;
+         for(int i=0;i<n;i++){
+             val*=mid;
+             if(val>m) break;
+         }
+         
+         if(val==m){
+             return  mid;
+         }else if(val>m){
+             high=mid-1;
+         }else{
+             low=mid+1;
+         }
+     }
+     return -1;
     }
 }
