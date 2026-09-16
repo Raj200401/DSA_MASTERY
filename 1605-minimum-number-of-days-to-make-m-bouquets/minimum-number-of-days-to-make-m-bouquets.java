829e561327dc45b9722c1bc8914a1660ffbe7cc9
Raj <codingfreak202527@gmail.com>
2026-09-16 12:23:15 +0530

    Time: 17 ms (70.73%) | Memory: 82.6 MB (66.90%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,43 +1,42 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
         int n=bloomDay.length;
-        int max=0;
-        int min=Integer.MAX_VALUE;
-        for(int num:bloomDay){
-            max=Math.max(max,num);
-            min=Math.min(min,num);
-        }
-        int low=min;
-        int high=max;
         if(n<(long)m*k) return -1;
-
-        while(low<=high){
-        int mid=low+(high-low)/2;
-
-        if(possible(bloomDay,m,k,mid)){
-            high=mid-1;
-        }else{
-            low=mid+1;
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        for(int i=0;i<n;i++){
+            min=Math.min(bloomDay[i],min);
+            max=Math.max(bloomDay[i],max);
         }
-        }
-        return low;
-    }
+            int low=min;
+            int high=max;
+            while(low<=high){
+               int mid=(low+high)/2;
 
-    public boolean possible(int[]bloomDay,int m,int k,int day){
-        int count=0;
-        int bouquet=0;
-        for(int i=0;i<bloomDay.length;i++){
-            if(bloomDay[i]<=day){
-                count++;
-            }else{
-                bouquet+=(count/k);
-                count=0;
+               if(possible(bloomDay,m,k,mid)){
+                high=mid-1;
+               }else{
+                low=mid+1;
+               }
+
             }
+            return low;
+        
+}
+public boolean possible(int[]bloomDay,int m,int k,int day){
+    int count=0;
+    int bouquet=0;
+    for(int i=0;i<bloomDay.length;i++){
+        if(bloomDay[i]<=day){
+            count++;
+        }else{
+            bouquet+=(count/k);
+            count=0;
         }
-        bouquet+=(count/k);
-
-        if(bouquet>=m) return true;
-
-        return false;
     }
+    bouquet+=(count/k);
+    if(bouquet>=m) return true;
+
+    return false;
+}
 }
